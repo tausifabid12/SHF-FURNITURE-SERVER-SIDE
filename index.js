5848f51cef03995ee8d43ab4f632dadee78ea780
tausifabid12 <tasifabid12@gmail.com>
2022-11-30 02:14:01 +0600

    bd connected

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ************ connecting mongodb **************** \\
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.brxmqep.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function dbConnect() {
+  try {
+    client.connect();
+    console.log("data base connected");
+  } catch (error) {
+    console.log(error.name, error.message);
+    res.send({
+      result: false,
+      error: error.message,
+    });
+  }
+}
+
+dbConnect();
+
 app.listen(port, () => {
   console.log("server is running");
 });
